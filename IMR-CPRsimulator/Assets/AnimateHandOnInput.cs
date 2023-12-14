b6f6f2a0e4f309c49942f6b836f2dd0a435d8aaa
Teodor Ivanusca <ITeod@DESKTOP-M8DH10A>
2023-12-14 20:21:34 +0200

    Maybe this time

---
+++ IMR-CPRsimulator/Assets/AnimateHandOnInput.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+
+    public InputActionReference pinchAnimationAction;
+    public InputActionReference gripAnimationAction;
+    public Animator handAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
