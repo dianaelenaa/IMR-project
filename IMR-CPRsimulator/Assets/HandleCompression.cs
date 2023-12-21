dd94e14929a3070c1ae8613ff2fc9ee6ce1f1ea0
Teodor Ivanusca <ITeod@DESKTOP-M8DH10A>
2023-12-21 17:05:06 +0200

    Add basic compression

---
+++ IMR-CPRsimulator/Assets/HandleCompression.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleCompression : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("LeftHand"))
+        {
+            Debug.Log("Compression with left hand");
+        }
+
+        if (collision.gameObject.CompareTag("RightHand"))
+        {
+            Debug.Log("Compression with right hand");
+        }
+    }
+}
